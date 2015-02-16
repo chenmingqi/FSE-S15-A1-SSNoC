98f479ff4b65ec568b7ed69938eed08af061b47b
chenmingqi <mingqic@andrew.cmu.edu>
2015-02-16 15:54:46 -0500

    passport login success

--- models/models.js
+++
@@ -1,8 +0,0 @@
-var orm = require("orm");
-
-orm.connect("sqlite://FSEProject.db", function(err, db){
-        // db is now available to use! ^__^
-    var Person = db.define('person', {
-        name: String
-    });
-});
--- routes/index.js
+++ routes/index.js
@@ -21,24 +21,71 @@ router.get('/logout/:username', function(req, res) {
 });
 
 
-router.post('/login', function(req, res) {
-  var username = req.body.username;
-  var password = req.body.password;
-  var confirm = req.body.confirm;
+// router.post('/login', function(req, res) {
+//   var username = req.body.username;
+//   var password = req.body.password;
+//   var confirm = req.body.confirm;
 
-  user.login(username,password,confirm, function(result) {
-    if(result.message) {
-      res.render(result.page, {message: result.message});
-    }
-    else {
-      user.getStatus(function(userlist) {
-        res.render(result.page, {name: result.username, on: JSON.stringify(userlist.on), off: JSON.stringify(userlist.off)} );
-      });
-    }
+//   user.login(username,password,confirm, function(result) {
+//     if(result.message) {
+//       res.render(result.page, {message: result.message});
+//     }
+//     else {
+//       user.getStatus(function(userlist) {
+//         res.render(result.page, {name: result.username, on: JSON.stringify(userlist.on), off: JSON.stringify(userlist.off)} );
+//       });
+//     }
 
+//   });
+// });
+
+
+//passport login
+var crypto = require('crypto');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('fse.db');
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+// function hashPassword(password, salt) {
+//   var hash = crypto.createHash('sha256');
+//   hash.update(password);
+//   hash.update(salt);
+//   return hash.digest('hex');
+// }
+passport.use(new LocalStrategy(function(username, password, done) {
+  db.get('SELECT salt FROM users WHERE username = ?', username, function(err, row) {
+    console.log(username);
+    console.log(password);
+    if (!row) return done(null, false);
+    // var hash = hashPassword(password, row.salt);
+    db.get('SELECT username, id FROM users WHERE username = ? AND password = ?', username, password, function(err, row) {
+      if (!row) return done(null, false);
+      return done(null, row);
+    });
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  return done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  db.get('SELECT id, username FROM users WHERE id = ?', id, function(err, row) {
+    if (!row) return done(null, false);
+    return done(null, row);
   });
 });
 
+router.post('/passport_login',
+  passport.authenticate('local', {
+    successRedirect: '/loginSuccess',
+    failureRedirect: '/loginFailure'
+  })
+);
 
 
 module.exports = router;
