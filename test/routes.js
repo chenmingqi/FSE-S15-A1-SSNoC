e0053517217d213a5a646358dece3bdeae50ae7f
monkerek <monkerek@hotmail.com>
2015-03-03 11:13:25 -0500

    add unit test for login

--- test/routes.js
+++ test/routes.js
@@ -4,11 +4,32 @@ var request = require('supertest');
 var models  = require('../models');
 var app = require('../app');
 var url = 'http://localhost:3000';
+var superagent = require('superagent');
+var agent = superagent.agent();
+
+
+
 
 describe('Homepage', function() {
   it('should not report error when browsing homepage', function(done) {
     request(url)
     .get('/')
     .expect(200, done);
-  })
-})
+  });
+});
+
+
+describe('Login', function() {
+  it('should log in successfully', function(done) {
+    agent
+    .post(url + '/login')
+    .send({ username: 'Derek', password: 'password' })
+    .end(function(err, res) {
+      if(err) {
+        throw err;
+      }
+      res.should.have.property('status', 200);
+      done();
+    });
+  });
+});
